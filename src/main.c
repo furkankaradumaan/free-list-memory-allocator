5289354260f40fa524a8d3beb0c9342a4188136b
Furkan Karaduman <furkaankaraduman@gmail.com>
2026-07-16 19:23:26 +0300

    Write a sample program using allocator
    I wrote a simple student record program that
    allocates an array and student structs
    using our 'allocator'.

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "../include/allocator.h"
+
+#define MAX_NAME 50
+
+typedef struct Student {
+        char name[MAX_NAME + 1];
+        float gpa;
+} Student;
+
+int main() {
+        memory_init();
+
+        const int student_count = 5;
+        Student **students = allocate(sizeof(Student *) * student_count);
+
+        for (int st = 1; st <= student_count; st++) {
+                Student *student = allocate(sizeof(Student));
+
+                printf("Enter name for student %d: ", st);
+                scanf("%51[^\n]", student->name);
+                printf("Enter gpa for student %d: ", st);
+                scanf("%f", &student->gpa);
+                while (getchar() != '\n');
+
+                students[st - 1] = student;
+        }
+
+        draw_memory();
+        return 0;
+}
